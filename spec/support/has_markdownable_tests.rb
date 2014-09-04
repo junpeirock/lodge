480cb50ad1a2c07eff6f390fd13ec3c5fb181146
kakusuke <bananafishmonger+github@gmail.com>
2014-09-04 21:52:14 +0900

    add test

--- spec/support/has_markdownable_tests.rb
+++ spec/support/has_markdownable_tests.rb
@@ -1,4 +1,6 @@
 RSpec.shared_examples "having markdownable" do |attr|
+  using RSpec::Parameterized::TableSyntax
+
   setter = "#{attr}="
   attr_html = "#{attr}_html"
 
@@ -6,16 +8,16 @@ RSpec.shared_examples "having markdownable" do |attr|
 
   describe attr_html do
 
-      where(:md, :html) do
-        [
-          ['#title', '<h1 id="title">title</h1>']
-        ]
-      end
-  
-      with_them do
-        before { model.send setter, md }
-        its(attr_html) { should match html }
-      end
+    where(:md, :html) do
+      '#title' | '<h1 id="title">title</h1>'
+      "```ruby:filename.rb\ntest\n```" | /<div class="code-filename">filename\.rb/
+      "```ruby:filename.rb\ntest\n```" | /class="CodeRay".*<pre>test/m
+    end
+
+    with_them do
+      before { model.send setter, md }
+      its(attr_html) { should match html }
+    end
   end
 
 end
