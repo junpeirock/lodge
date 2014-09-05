da5372488b90ab978444741515357a525bb4ed8b
kakusuke <bananafishmonger+github@gmail.com>
2014-09-06 03:00:56 +0900

    add test

--- spec/support/has_markdownable_tests.rb
+++ spec/support/has_markdownable_tests.rb
@@ -3,15 +3,17 @@ RSpec.shared_examples "having markdownable" do |attr|
 
   setter = "#{attr}="
   attr_html = "#{attr}_html"
+  attr_html_with_toc = "#{attr}_html_with_toc"
 
   subject (:model) { described_class.new }
 
   describe attr_html do
 
     where(:md, :html) do
-      '#title' | '<h1 id="title">title</h1>'
-      "```ruby:filename.rb\ntest\n```" | /<div class="code-filename">filename\.rb/
-      "```ruby:filename.rb\ntest\n```" | /class="CodeRay".*<pre>test/m
+      '#title'                    | '<h1 id="title">title</h1>'
+      "```ruby:name.rb\n<a>\n```" | /<div class="code-filename">name\.rb/
+      "```ruby:name.rb\n<a>\n```" | /class="CodeRay".*<pre>&lt;a&gt;/m
+      "foo\nbar"                  | /foo<br>.*bar/m
     end
 
     with_them do
@@ -20,4 +22,14 @@ RSpec.shared_examples "having markdownable" do |attr|
     end
   end
 
+  describe attr_html_with_toc do
+    where(:md, :html) do
+      "#h1\n##h2" | /<li>.*h1.*<ul>.*<li>.*h2/m
+    end
+
+    with_them do
+      before { model.send setter, md }
+      its(attr_html_with_toc) { should match html }
+    end
+  end
 end
